6242759f780af00721fe41e7722084511a1886ea
Matias <matias.turra@gmail.com>
2022-01-21 09:49:05 -0300

    finished options

--- sidebar.js
+++ sidebar.js
@@ -3,7 +3,8 @@ let arrow = document.querySelectorAll(".arrow");
 for (let index = 0; index < arrow.length; index++) {
     arrow[index].addEventListener("click", (e)=>{
         let arrowParent = e.target.parentElement.parentElement;
-        console.log(arrowParent);
         arrowParent.classList.toggle("showMenu");
     });
 }
+
+let sidebar = document.querySelector(".sidebar");
